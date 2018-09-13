1cbdb883e37f050e36093da195b8676f98f72d45
artineh <43155892+artineh@users.noreply.github.com>
2018-09-13 21:41:58 +0400

    homework2

---
+++ homework2.js
@@ -0,0 +1,11 @@
+const a = 77;
+const b = 55;
+const c = a + b;
+const d = a - b;
+const e = "Joe";
+const f = 44;
+const g = e + f
+if ( d===22) { alert ("Hello") }
+ else { alert ("goodbye") }
+console.log( a + b + c)
+console.log(e + " " + "is happy")
